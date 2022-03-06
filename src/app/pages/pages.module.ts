edec28aeb845d0e60fc9855411a23184f7a31739
johnnami <joaonetonamikaze@gmail.com>
2022-03-05 22:21:55 -0300

    Homepage finished

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//module routing
+import { RoutingModule } from './routing.module';
+
+
+//module
+import { SharedModule } from '../shared/shared.module';
+
+//pages
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    DetailsComponent
+  ],
+  imports: [
+    CommonModule,
+    RoutingModule,
+    SharedModule
+  ]
+})
+export class PagesModule { }
