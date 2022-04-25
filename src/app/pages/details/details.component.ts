1ab0393a60e756fa01078ea07fac425fc7ad41fd
johnnami <joaonetonamikaze@gmail.com>
2022-04-24 23:52:11 -0300

    finished details

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -15,27 +15,32 @@ export class DetailsComponent implements OnInit {
   private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
   private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
+  public pokemon: any;
+  public isLoading: boolean = false;
+  public apiError: boolean = false;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private pokeApiService: PokeApiService
-  ) { }
-
+  ) { };
 
   ngOnInit(): void {
-    this.pokemon;
+    this.getPokemon();
   }
 
-  get pokemon(){
+  public getPokemon(){
     const id = this.activatedRoute.snapshot.params['id'];
     const pokemon = this.pokeApiService.apiGetPokemon(`${this.urlPokemon}/${id}`);
     const name = this.pokeApiService.apiGetPokemon(`${this.urlName}/${id}`);
 
     return forkJoin([pokemon, name]).subscribe(
       res => {
-        console.log(res);
+        this.pokemon = res;
+        this.isLoading = true;
+      },
+      error => {
+        this.apiError = true;
       }
     );
-
-
-  }
-}
+  };
+};
